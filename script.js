a6a8535cfa155f1ec9fceaa2f6f6f50f658d5c53
Student <student@RU-102.local>
2016-02-04 10:56:05 -0700

    quoteR

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// //----------Module------------------
+// angular.module('Name', []);
+
+// //----------Factory------------------
+// angular.module('Name').factory('factoryName', [function(){
+
+// }]);
+
+// //----------Contoller------------------
+// angular.module('Name').contoller('contollerName', ['$scope', 'factoryName', function($scope, factoryName) {
+
+// }]);
+
+angular.module('quoteR', []);
+
+angular.module('quoteR').factory('quoteFactory', [function(){
+	var
+}]);
+
+angular.module('quoteR').contoller('quotoller', ['$scope', 'quoteFactory', function($scope, quoteFactory) {
+	var
+}]);
